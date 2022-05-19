a9077c55ff1e245a7ba147f45c3f1d07d72cb820
jhs951101 <spg1101@naver.com>
2022-05-19 17:40:05 +0900

    m

--- src/pkg/TestHTTPGet.java
+++ src/pkg/TestHTTPGet.java
@@ -1,22 +1,18 @@
 package pkg;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class TestHTTPGet {
 	
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		
-		String URL = "https://api.odcloud.kr/api/15093373/v1/uddi:2d2989e6-8233-40f6-a4db-dbe92e979691";
+		String URL = "(URL �Է�)";
 		String TYPE = "GET";
-		//String USER_AGENT = "Mozilla/5.0";
-		//String DATA = "test data";
 		
-		String[] Parameters = {"page=1", "perPage=5",
-				"serviceKey=jJIP7tDlfl4QoksxclTp5r6cyl5%2Bn7WZnQUwsw3g5FXJSaXJyBRdYVyc2jEv5iuiXbwCKarsivrF63lpQxjxWA%3D%3D"};
+		String[] Parameters = {"page=1", "perPage=5"};
 		
 		if(Parameters != null) {
 			if(Parameters.length > 0) {
@@ -35,7 +31,6 @@ public class TestHTTPGet {
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
 		connection.setRequestMethod(TYPE);
-		//connection.setRequestProperty("User-Agent", USER_AGENT);
 
 		int responseCode = connection.getResponseCode();
 
@@ -50,6 +45,6 @@ public class TestHTTPGet {
         bufferedReader.close();
 
         String response = stringBuffer.toString();
-        System.out.println(response);  // JSON Array 형태로 출력
+        System.out.println(response);
     }
 }
